cdcb225ed3ba0ccbb662c8bd7d57398d04a18e25
Priyansh Maurya <priyansh.maurya@gmail.com>
2024-03-19 03:34:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const Header = React.lazy(() => import("MicroFrontend/Header"));
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Shell Application
+      <Header title="Hey there" />
     </div>
   );
 }
-
-export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html",
+});
+module.exports = {
+  mode: "development",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3001,
+    historyApiFallback: {
+      index: "/public/index.html",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    htmlPlugin,
+    new ModuleFederationPlugin({
+      name: "ShellApplication",
+      filename: "remoteEntry.js",
+      remotes: {
+        MicroFrontend: "MicroFrontend@http://localhost:3002/remoteEntry.js",
+      },
+      shared: {
+        "@ant-design/icons": {
+          import: false,
+          requiredVersion: "^4.8.2",
+          strictVersion: true,
+        },
+      },
+    }),
+  ],
+};
